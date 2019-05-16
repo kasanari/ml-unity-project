31ef929c8a31fb362276b35540330cbf8c0bf300
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-05-16 17:36:45 +0200

    update to python script

--- ParameterSearch/parameterSearch.py
+++ ParameterSearch/parameterSearch.py
@@ -9,7 +9,7 @@ import winsound
 root_dir = "C:/Users/Jakob/Documents/GitHub/ml-unity-project" # Change this to your script folder
 env_name = "Maze0Multi/Smartball"
 brain_name = "RollerBallRayBrain"
-out_file_name = "big_list_of_results"
+out_file_name = "results"
 base_run_id = "Python"
 big_list_of_results = None
 
@@ -19,9 +19,9 @@ batch_size = 1024
 beta = 5.0e-3
 buffer_size = 10240
 epsilon = 0.2
-gamma = 0.99
+gamma = 0.995
 hidden_units = 128
-lambd = 0.95
+lambd = 0.9125
 learning_rate = 3.0e-4
 max_steps = 5.0e4
 memory_size = 256
@@ -73,28 +73,46 @@ out_file = open(out_file_name + ".csv", 'w')
 # learning_rates = [i*1e-3 for i in learning_rates]
 # time_horizons = [32, 256, 480, 704, 928, 1152, 1376, 1600, 1824, 2048]
 
+number = 5
+gammas = {'name': 'gamma', 'values': np.linspace(0.8, 0.995, number)}
+lambdas = {'name': 'lambd', 'values': np.linspace(0.9, 0.95, number)}
+batch_sizes = {'name': 'batch_size', 'values': np.linspace(512, 5120, number)}
+buffer_sizes = {'name': 'buffer_size', 'values': [i*10 for i in batch_sizes['values'].tolist()]}
+number_of_layers = {'name': 'num_layers', 'values': [1, 2, 3]}
+hidden_units = {'name': 'hidden_units', 'values': np.linspace(32, 512, number)}
+num_epochs = {'name': 'num_epochs', 'values': np.linspace(3, 11, number)}
+learning_rates = {'name': 'learning_rate', 'values': np.logspace(-5, -3, number)}
+time_horizons = {'name': 'time_horizon', 'values': np.linspace(32, 2048, number)}
+max_steps = {'name': 'max_steps', 'values': np.linspace(5e5, 1e7, number)}
+betas = {'name': 'beta', 'values': np.logspace(-4, -2, number)}
+epsilons = {'name': 'epsilon', 'values': np.linspace(0.1, 0.3, number)}
+normalizes = {'name': 'normalize', 'values': [True, False]}
 
 # -----------------------------------------
+
+current_param = hidden_units
 
 # This is the main loop which will run several training scenarios
 # It will run for as many times as specified and collect the last result tuple of each run.
 # Results are saved each iteration
 
+# betas = [0.0001, 0.0012, 0.0023, 0.0034, 0.0045, 0.0056, 0.0067, 0.0078, 0.0089, 0.0100]
 
+name = current_param['name']
+values = current_param['values'] # .tolist()
 
-betas = [0.0001, 0.0012, 0.0023, 0.0034, 0.0045, 0.0056, 0.0067, 0.0078, 0.0089, 0.0100]
-
-for i in range(len(betas)): # <-- Change this to whatever the length of the main parameter list is, eg. range(len(batch_sizes))
+for i in range(len(values)): # <-- Change this to whatever the length of the main parameter list is, eg. range(len(batch_sizes))
 
     # Change parameters here
     # Example:
     # settings["default"]["batch_size"] = 1000 + i
 
-    # settings["default"]["batch_size"] = batch_sizes[i]
-    # settings["default"]["buffer_size"] = buffer_sizes[i]
-    # settings["default"]["time_horizon"] = time_horizons[i]
+    # settings["default"]["batch_size"] = batch_sizes['values'].tolist()[i]
+    # settings["default"]["buffer_size"] = buffer_sizes['values'][i]
+    # settings["default"]["time_horizon"] = time_horizons[i]W
 
-    settings["default"]["beta"] = betas[i]
+    value = values[i]
+    settings["default"][name] = value
 
     # Write settings to file
     yaml_to_write = yaml.dump(settings)
@@ -118,8 +136,8 @@ for i in range(len(betas)): # <-- Change this to whatever the length of the main
     
     out_file.write(big_list_of_results.to_csv())
 
-
 duration = 1000  # milliseconds
 freq = 440  # Hz
 winsound.Beep(freq, duration)
 out_file.close()
+subprocess.run(f'7z a -tzip {name}.zip "summaries" "models"', shell=True, check=True) 
