0cbde00804e044fe5909cfc7a1a74b9108736340
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-05-17 10:05:27 +0200

    reverted while keeping some things

--- ParameterSearch/parameterSearch.py
+++ ParameterSearch/parameterSearch.py
@@ -4,9 +4,11 @@ import pandas as pd
 import os
 import numpy as np
 import winsound
+import shutil
 
-#Script Settings
-root_dir = "C:/Users/Jakob/Documents/GitHub/ml-unity-project" # Change this to your script folder
+# Script Settings
+# Change this to your script folder
+root_dir = "C:/Users/Jakob/Documents/GitHub/ml-unity-project"
 env_name = "Maze0Multi/Smartball"
 brain_name = "RollerBallRayBrain"
 out_file_name = "results"
@@ -37,9 +39,9 @@ curiosity_strength = 0.01
 curiosity_enc_size = 128
 
 settings = {"default": {'trainer': trainer, 'batch_size': batch_size, 'beta': beta, 'buffer_size': buffer_size, 'epsilon': epsilon,
-                         'gamma':gamma, 'hidden_units': hidden_units, "lambd":lambd, "learning_rate": learning_rate, "max_steps": max_steps, "memory_size": memory_size,
-                         'normalize':normalize, 'num_epoch':num_epoch, 'num_layers':num_layers, 'time_horizon':time_horizon, 'sequence_length':sequence_length,
-                         'summary_freq':summary_freq, 'use_recurrent':use_recurrent, 'use_curiosity':use_curiosity, 'curiosity_strength':curiosity_strength, 'curiosity_enc_size':curiosity_enc_size}}
+                        'gamma': gamma, 'hidden_units': hidden_units, "lambd": lambd, "learning_rate": learning_rate, "max_steps": max_steps, "memory_size": memory_size,
+                        'normalize': normalize, 'num_epoch': num_epoch, 'num_layers': num_layers, 'time_horizon': time_horizon, 'sequence_length': sequence_length,
+                        'summary_freq': summary_freq, 'use_recurrent': use_recurrent, 'use_curiosity': use_curiosity, 'curiosity_strength': curiosity_strength, 'curiosity_enc_size': curiosity_enc_size}}
 
 
 os.chdir(root_dir)
@@ -54,11 +56,12 @@ if os.path.isdir("./summaries"):
      exit()
 
 # If the output file already exists, create another one with an incremented name
-out_file_exists = os.path.isfile(out_file_name+ ".csv")
+out_file_exists = os.path.isfile(out_file_name + ".csv")
 i = 0
 while(out_file_exists):
-    out_file_name += str(i)
-    out_file_exists = os.path.isfile(out_file_name+ ".csv")
+    out_file_name = out_file_name[:-1] + str(i)
+    out_file_exists = os.path.isfile(out_file_name + ".csv")
+    i += 1
 
 out_file = open(out_file_name + ".csv", 'w')
 
@@ -77,20 +80,22 @@ number = 5
 gammas = {'name': 'gamma', 'values': np.linspace(0.8, 0.995, number)}
 lambdas = {'name': 'lambd', 'values': np.linspace(0.9, 0.95, number)}
 batch_sizes = {'name': 'batch_size', 'values': np.linspace(512, 5120, number)}
-buffer_sizes = {'name': 'buffer_size', 'values': [i*10 for i in batch_sizes['values'].tolist()]}
+#buffer_sizes = {'name': 'buffer_size', 'values': [i*10 for i in batch_sizes['values'].tolist()]}
 number_of_layers = {'name': 'num_layers', 'values': [1, 2, 3]}
 hidden_units = {'name': 'hidden_units', 'values': np.linspace(32, 512, number)}
 num_epochs = {'name': 'num_epochs', 'values': np.linspace(3, 11, number)}
-learning_rates = {'name': 'learning_rate', 'values': np.logspace(-5, -3, number)}
-time_horizons = {'name': 'time_horizon', 'values': np.linspace(32, 2048, number)}
-max_steps = {'name': 'max_steps', 'values': np.linspace(5e5, 1e7, number)}
+learning_rates = {'name': 'learning_rate',
+                  'values': np.logspace(-5, -3, number)}
+time_horizons = {'name': 'time_horizon',
+                 'values': np.linspace(32, 2048, number)}
+# max_steps = {'name': 'max_steps', 'values': np.linspace(5e5, 1e7, number)}
 betas = {'name': 'beta', 'values': np.logspace(-4, -2, number)}
 epsilons = {'name': 'epsilon', 'values': np.linspace(0.1, 0.3, number)}
 normalizes = {'name': 'normalize', 'values': [True, False]}
 
 # -----------------------------------------
 
-current_param = hidden_units
+current_param = gammas
 
 # This is the main loop which will run several training scenarios
 # It will run for as many times as specified and collect the last result tuple of each run.
@@ -99,20 +104,18 @@ current_param = hidden_units
 # betas = [0.0001, 0.0012, 0.0023, 0.0034, 0.0045, 0.0056, 0.0067, 0.0078, 0.0089, 0.0100]
 
 name = current_param['name']
-values = current_param['values'] # .tolist()
+try:
+    values = current_param['values'].tolist()
+except:
+    values = current_param['values']
 
-for i in range(len(values)): # <-- Change this to whatever the length of the main parameter list is, eg. range(len(batch_sizes))
+for i in range(len(values)):
 
-    # Change parameters here
-    # Example:
-    # settings["default"]["batch_size"] = 1000 + i
+    settings["default"][name] = values[i]
 
-    # settings["default"]["batch_size"] = batch_sizes['values'].tolist()[i]
-    # settings["default"]["buffer_size"] = buffer_sizes['values'][i]
-    # settings["default"]["time_horizon"] = time_horizons[i]W
-
-    value = values[i]
-    settings["default"][name] = value
+    # To ensure the condition that buffer_size = 10*batch_size
+    settings["default"]["buffer_size"] = 10 * \
+        settings["default"]["batch_size"]
 
     # Write settings to file
     yaml_to_write = yaml.dump(settings)
@@ -132,12 +135,23 @@ for i in range(len(values)): # <-- Change this to whatever the length of the mai
     if (big_list_of_results is None):
         big_list_of_results = results
     else:
-        big_list_of_results = pd.concat([big_list_of_results, results], ignore_index=True)
-    
-    out_file.write(big_list_of_results.to_csv())
+        big_list_of_results = big_list_of_results.append(results, ignore_index=True)
+        big_list_of_results = big_list_of_results.reset_index(drop=True)
+        # End of a single parameter value
 
+    with open(f'{name}.csv', 'w') as f: # save the results for all parameters
+        f.write(big_list_of_results.to_csv())
+        f.close
+    
+    subprocess.run(
+        f'7z a -tzip output_archives/{name}.zip "summaries" "models"', shell=True, check=True)
+    # shutil.rmtree('models')
+    # shutil.rmtree('summaries')
+    # End of a single parameter
+
+# Make a noise to indicate that we are done
 duration = 1000  # milliseconds
 freq = 440  # Hz
 winsound.Beep(freq, duration)
 out_file.close()
-subprocess.run(f'7z a -tzip {name}.zip "summaries" "models"', shell=True, check=True) 
+# End of program
